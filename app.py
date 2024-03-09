41f3a6b08e833c867a67072e4894a78b5ac7eb8e
piyush <22341@iiitu.ac.in>
2024-03-09 11:25:41 +0530

    new commit

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def recommend_recipes(user_history_json_file):
 
 app = Flask(__name__) 
 
-@app.route('/recommend', methods=['POST'])
+@app.route('/', methods=['POST'])
 def recommend():
     user_history = request.get_json()
     print(type(user_history))
